0437ea7088baca85c7d4427bcb8463979f92ab85
seeml-mo <panhuangyu088@gmail.com>
2026-04-13 15:34:33 +0800

    JUnit Test problem

--- src/test/java/tradelog/logic/parser/ParserUtilTest.java
+++ src/test/java/tradelog/logic/parser/ParserUtilTest.java
@@ -10,13 +10,13 @@ import tradelog.exception.TradeLogException;
 public class ParserUtilTest {
 
     @Test
-    public void parseStrategy_knownShortcut_returnsExpandedName() {
+    public void parseStrategy_knownShortcut_returnsExpandedName() throws Exception {
         assertEquals("Breakout", ParserUtil.parseStrategy("BB"));
         assertEquals("Major Trend Reversal", ParserUtil.parseStrategy("mtr"));
     }
 
     @Test
-    public void parseStrategy_knownFullNameCaseVariant_returnsCanonicalName() {
+    public void parseStrategy_knownFullNameCaseVariant_returnsCanonicalName() throws Exception {
         assertEquals("Breakout", ParserUtil.parseStrategy("breakout"));
         assertEquals("Major Trend Reversal", ParserUtil.parseStrategy("  major   trend reversal "));
     }
@@ -27,7 +27,49 @@ public class ParserUtilTest {
     }
 
     @Test
+    public void parseStrategy_emptyStrategy_throwsTradeLogException() {
+        assertThrows(TradeLogException.class, () -> ParserUtil.parseStrategy(""));
+        assertThrows(TradeLogException.class, () -> ParserUtil.parseStrategy("   "));
+    }
+
+    @Test
     public void canonicalizeStoredStrategy_unknownStrategy_returnsTrimmedInput() {
         assertEquals("Custom Strategy", ParserUtil.canonicalizeStoredStrategy("  Custom Strategy  "));
+    }
+
+    @Test
+    public void parseTicker_validTicker_returnsUppercase() throws Exception {
+        assertEquals("AAPL", ParserUtil.parseTicker("aapl"));
+        assertEquals("MSFT", ParserUtil.parseTicker("  msft  "));
+    }
+
+    @Test
+    public void parseTicker_emptyTicker_throwsTradeLogException() {
+        assertThrows(TradeLogException.class, () -> ParserUtil.parseTicker(""));
+        assertThrows(TradeLogException.class, () -> ParserUtil.parseTicker("   "));
+    }
+
+    @Test
+    public void parsePrice_validPrice_returnsDouble() throws Exception {
+        assertEquals(150.5, ParserUtil.parsePrice("150.5", "Entry"));
+    }
+
+    @Test
+    public void parsePrice_invalidPrice_throwsTradeLogException() {
+        assertThrows(TradeLogException.class, () -> ParserUtil.parsePrice("abc", "Entry"));
+        assertThrows(TradeLogException.class, () -> ParserUtil.parsePrice("-10", "Entry"));
+        assertThrows(TradeLogException.class, () -> ParserUtil.parsePrice("", "Entry"));
+    }
+
+    @Test
+    public void parseDirection_validDirection_returnsFormatted() throws Exception {
+        assertEquals("Long", ParserUtil.parseDirection("long"));
+        assertEquals("Short", ParserUtil.parseDirection("SHORT"));
+    }
+
+    @Test
+    public void parseDirection_invalidDirection_throwsTradeLogException() {
+        assertThrows(TradeLogException.class, () -> ParserUtil.parseDirection("up"));
+        assertThrows(TradeLogException.class, () -> ParserUtil.parseDirection(""));
     }
 }
